6a712e833a59665d41c8e46a465102033105c106
Julian Cheng <julian.cheng@gmail.com>
2014-05-22 02:53:53 -0500

    modified seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,40 +14,40 @@ weights = [20, 30, 40, 50, 60]
 upper_body = Workout.create(name: "Upper Body")
 lower_body = Workout.create(name: "Lower Body")
 
-CSV.foreach("workout.csv", :headers => true, :header_converters => :symbol) do |row|
-		Exercise.all.each do |exercise|
-			:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
-			if row[:workout] == "Upper Body"
-				exercise.update_attributes(workout_id: upper_body.id)
-			elsif row[:workout] == "Lower Body"
-				exercise.update_attributes(workout_id: upper_body.id)
-			end
-		end
-end	
-
 # CSV.foreach("workout.csv", :headers => true, :header_converters => :symbol) do |row|
-# 		if row[:workout] == "Upper Body"
-# 			exercise = Exercise.create(
-# 				:name => row[:name],
-# 				:reps => row[:reps],
-# 				:sets => row[:sets],
-# 				:rest => row[:rest],
-# 				:description => row[:description],
-# 				:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
-# 			exercise.update_attributes(workout_id: upper_body.id)
-# 			# Exercise.find_by_name(row[:name]).update_attributes(workout_id: upper_body.id)
-# 			# Exercise.find_by_name(row[:name]).update_attributes(:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
-# 		else
-# 			exercise = Exercise.create(
-# 				:name => row[:name],
-# 				:reps => row[:reps],
-# 				:sets => row[:sets],
-# 				:rest => row[:rest],
-# 				:description => row[:description],
-# 				:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
-# 			exercise.update_attributes(workout_id: lower_body.id)
+# 		Exercise.all.each do |exercise|
+# 			:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
+# 			if row[:workout] == "Upper Body"
+# 				exercise.update_attributes(workout_id: upper_body.id)
+# 			elsif row[:workout] == "Lower Body"
+# 				exercise.update_attributes(workout_id: upper_body.id)
+# 			end
 # 		end
-# end
+# end	
+
+ CSV.foreach("workout.csv", :headers => true, :header_converters => :symbol) do |row|
+ 		if row[:workout] == "Upper Body"
+ 			exercise = Exercise.create(
+ 				:name => row[:name],
+ 				:reps => row[:reps],
+ 				:sets => row[:sets],
+				:rest => row[:rest],
+ 				:description => row[:description],
+ 				:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
+ 			exercise.update_attributes(workout_id: upper_body.id)
+# 			# Exercise.find_by_name(row[:name]).update_attributes(workout_id: upper_body.id)
+			# Exercise.find_by_name(row[:name]).update_attributes(:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
+ 		else
+ 			exercise = Exercise.create(
+ 				:name => row[:name],
+				:reps => row[:reps],
+ 				:sets => row[:sets],
+				:rest => row[:rest],
+ 				:description => row[:description],
+ 				:image => "https://s3.amazonaws.com/swoletrain/#{row[:name].gsub(/ /,'+')}"+".JPG")
+ 			exercise.update_attributes(workout_id: lower_body.id)
+ 		end
+end
 
 
 # 10.times do 
